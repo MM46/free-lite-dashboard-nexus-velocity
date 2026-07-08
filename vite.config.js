bba2c9b04c4df01b2e52ce6a38c5b6a8d4250df8
MM46 <marianmunoz_@Hotmail.com>
2026-07-08 14:31:40 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import base44 from '@base44/vite-plugin'
 
 // https://vite.dev
 export default defineConfig({
-  base: '/free-lite-dashboard-nexus-velocity/',
   plugins: [
     base44({
       legacySDKImports: false,
